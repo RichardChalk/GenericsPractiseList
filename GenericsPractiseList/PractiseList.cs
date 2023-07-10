50a54657cc99a5e636d99fb89715bf54da69e1ab
Richard Chalk <barmybrit@gmail.com>
2023-07-10 08:19:03 +0200

    Add project files.

---
+++ GenericsPractiseList/PractiseList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPractiseList
+{
+    public class PracticeList<T> 
+    {
+        private T[] items;
+        
+        public PracticeList()
+        {
+            items = new T[0];
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public T[] GetItems()
+        {
+            return items;
+        }
+
+
+        public void Add(T newItem)
+        {
+            // Make a new bigger array with room
+            // to store the new item.
+            T[] newItems = new T[items.Length + 1];
+            // Copy all of the old items over to the new array.
+            for (int index = 0; index < items.Length; index++)
+                newItems[index] = items[index];
+            // Put the new item at the end.
+            newItems[newItems.Length - 1] = newItem;
+            // Update our instance variable to hold this new
+            // array instead of the old array.
+            items = newItems;
+        }
+    }
+}
---
+++ GenericsPractiseList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericsPractiseList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till min PractisList class!");
+
+            // Int
+            var myPractiseListClassInt = new PracticeList<int>();
+            myPractiseListClassInt.Add(1);
+            myPractiseListClassInt.Add(2);
+            myPractiseListClassInt.Add(3);
+
+            var itemsInt = myPractiseListClassInt.GetItems();
+
+            for (int i = 0; i < itemsInt.Length; i++)
+            {
+                Console.WriteLine($"int List index position {i}: {itemsInt[i]}");
+            }
+
+            // String
+            var myPractiseListClassString = new PracticeList<string>();
+            myPractiseListClassString.Add("Richard");
+            myPractiseListClassString.Add("Stefan");
+            myPractiseListClassString.Add("Sebastian");
+
+            var itemString = myPractiseListClassString.GetItem(2);
+
+            Console.WriteLine($"string List {2+1} item: {itemString}");
+        }
+    }
+}
